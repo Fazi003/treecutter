190a2e158a987f16c2fce543506bd188ba004132
Fazi003 <hunger@gmail.hu>
2023-09-17 19:17:55 +0200

    Elso

---
+++ script.js
@@ -0,0 +1,11 @@
+let login = document.querySelector('.login_form');
+
+document.querySelector('#account-btn').onclick = () =>{
+    login.classList.toggle('active');
+}
+
+let buttonLogin = document.querySelector('.login_form');
+
+document.querySelector('#signin').onclick = ()=>{
+    buttonLogin.classList.toggle('active');
+}
